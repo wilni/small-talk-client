6483f8905e6526b33744b6664c8c40d3134b5436
Will Tavarez <will@Wills-MacBook-Pro.local>
2022-08-02 21:58:17 -0400

    queried db for user status and to post user to db

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer.js';
 import Connections from './components/Connections/Connections.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
 
 
 
@@ -14,32 +15,45 @@ function App() {
     return (<div>Loading ...</div>);
   }
 
-  if(!isAuthenticated){
+  if (!isAuthenticated) {
     return (
       <>
-      <Header isAuthenticated={isAuthenticated} />
-    <div>Please sign in</div>
-</>
+        <Header isAuthenticated={isAuthenticated} />
+        <div>Please sign in</div>
+      </>
     )
-  }else {
+  } else {
     //check if there is user with email in database 
     // if !user send a post to back end 
+    axios.get(`http://localhost:8080/user/${user.email}`)
+    .then(res => {
+      if(res.data.sucess){
+        console.log("res form front end is true, user is already in database",res.data.sucess);
+      }else {
+        console.log("res form front end is false we need to add user to db",res.data.sucess);
+        axios.post(`http://localhost:8080/user/${user.email}`)
+        .then(res => {
+          console.log('res from post front end',res)
+        })
+      }
+      
+    })
   }
 
 
 
   return (
 
-      <Router>
+    <Router>
       <main className='App'>
-      <Header auth={isAuthenticated} />
-      <div className='card'>
-      <Switch>
-        <Route path={'/'}  component={Connections} />
-      </Switch>
-      </div>
-      <p>{JSON.stringify(user)}</p>
-      <Footer/>
+        <Header auth={isAuthenticated} />
+        <div className='card'>
+          <Switch>
+            <Route path={'/'} component={Connections} /> 
+          </Switch>
+        </div>
+        <p>{JSON.stringify(user)}</p>
+        <Footer />
       </main>
     </Router>
   );
--- src/components/Connections/Connections.js
+++ src/components/Connections/Connections.js
@@ -1,7 +1,12 @@
 import './Connections.scss';
+import React, { useState, useEffect } from 'react';
 
 
 function Connections(props) {
+
+
+
+
     return (
         <section className='chats'>
             <h3>HI, im a connection, click me to go to our chat </h3>
