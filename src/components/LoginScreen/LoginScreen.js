ad3653a2b503b36be104026f2a8b8f81d437f2e2
Will Tavarez <will@Wills-MBP.localdomain>
2022-08-11 14:29:25 -0400

    Login Screen animation

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -3,7 +3,7 @@ import './LoginScreen.scss';
 function LoginScreen(){
     return(
         <div className='login'>
-            <h3 className='login__title'>Please Log in to small talk</h3>
+            <h3 className='login__title blink'>Please Log in to Small Talk</h3>
         </div>
     )
 }
