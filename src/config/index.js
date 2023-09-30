a6ae3cbe70405b26506963b6cb2a3fd647ec00e0
Will Tavarez <will@Wills-MacBook-Pro.local>
2023-09-30 19:02:45 -0400

    config for heroku deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://small-talk-server.herokuapp.com'
+    ? 'https://small-talk-server2-7f51f0b33750.herokuapp.com'
     : 'http://localhost:8080';
